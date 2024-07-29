55aabd4be5dc95c444bc21182bb8df8548222860
Ali Altun <92324915+alibilgealtun@users.noreply.github.com>
2024-07-30 00:39:40 +0300

    removed taskdetails from router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './views/Home.vue';
-import TaskDetails from './components/TaskDetails.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
-  { path: '/tasks/:id', name: 'TaskDetails', component: TaskDetails },
 ];
 
 const router = createRouter({
